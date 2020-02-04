f5f9c002afac8a20b560ea4d8755f95f6c3a2865
heshida01 <31440693+heshida01@users.noreply.github.com>
2020-02-04 13:09:21 +0800

    Update mrmd2.0.py

--- mrmd2.0.py
+++ mrmd2.0.py
@@ -336,7 +336,7 @@ if __name__ == '__main__':
     tsne_scatter(csvfile)
 
 
-    pngpath = os.path.abspath('./Results')+os.sep+args.i+'.png'
+    pngpath = os.path.abspath('./Results')+os.sep+'.png'
     plt.savefig(pngpath)
     logger.info('Scatter charts visualized by t-SNE dataset has been saved in the {}.'.format(pngpath))
 
