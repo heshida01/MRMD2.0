5a4f7e633f1dde979825b0ebd2933c3442cffc6a
heshida01 <31440693+heshida01@users.noreply.github.com>
2022-12-18 21:21:57 +0900

    Update lasso.py

--- feature_selection/lasso.py
+++ feature_selection/lasso.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import Lasso
 import pandas as pd
 import numpy as np
+import 
 def lasso(file):
     dataset = pd.read_csv(file,engine='python').dropna(axis=1)
     features_name = dataset.columns.values.tolist()
@@ -8,13 +9,18 @@ def lasso(file):
 
     X = dataset[:, 1:]
     y = dataset[:, 0]
-
-    lasso = Lasso(alpha=.0001, random_state=1)
+    
+    lasso =  LogisticRegression(penalty="l1",solver="saga")
     lasso.fit(X, y)
-    # print(lasso.coef_)
-    ranks = {}
     result = [(x, y) for x, y in zip(features_name[1:], lasso.coef_)]
     result = sorted(result, key=lambda x: abs(x[1]), reverse=True)
+
+    #lasso = Lasso(alpha=.0001, random_state=1)
+    #lasso.fit(X, y)
+    # print(lasso.coef_)
+    #ranks = {}
+    #result = [(x, y) for x, y in zip(features_name[1:], lasso.coef_)]
+    #result = sorted(result, key=lambda x: abs(x[1]), reverse=True)
 
     return [x[0] for x in result if abs(x[1])>0.0000000000001]
 
@@ -24,6 +30,3 @@ def run(csvfile,logger):
 
     logger.info('lasso end.')
     return feature_list
-#
-# filepath =  r'J:\多设备共享\work\MRMD2.0-github\mixfeature_frequency_DBD.csv'
-# result = lasso(filepath)
