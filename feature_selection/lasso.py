b3056989b241d44c13f661b4ef43d7426439c4a2
heshida01 <31440693+heshida01@users.noreply.github.com>
2022-12-18 21:55:17 +0900

    Update lasso.py

--- feature_selection/lasso.py
+++ feature_selection/lasso.py
@@ -22,7 +22,7 @@ def lasso(file):
     #result = [(x, y) for x, y in zip(features_name[1:], lasso.coef_)]
     #result = sorted(result, key=lambda x: abs(x[1]), reverse=True)
 
-    return [x[0] for x in result if abs(x[1])>0.0000000000001]
+    return [x[0] for x in result1 ]
 
 def run(csvfile,logger):
     logger.info('lasso start...')
